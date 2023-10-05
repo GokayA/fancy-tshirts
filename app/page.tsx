a4804d77d52925af91dbba47afc9a77c8c990a6f
GokayA <arjenrobbennn@gmail.com>
2023-10-05 19:21:49 +0300

    added and fetched data with  sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+import { client } from '@/sanity/lib/client';
+import { groq } from 'next-sanity';
+
+interface Props {
+  _id: string;
+  _createdAt: Date;
+  name: string;
+  sku: string;
+  images: string;
+  currency: string;
+  price: number;
+  description: string;
+  slug: string;
+}
+
+export default async function Home() {
+  const products = await client.fetch<Props[]>(groq`*[_type == "product"] {
+    _id,
+    _createdAt,
+    name,
+    sku,
+    images,
+    currency,
+    price,
+    description,
+    "slug":slug.current
+  }`);
+  console.log(products);
   return (
     <div className="flex justify-center items-center p-40 w-full h-full ">
-      <div className="container ">Products</div>
+      <div className="container">
+        {products.length} Product {products.length === 1 ? '' : 's'}
+      </div>
+      <div>
+        {products.map((product) => (
+          <div key={product._id}>
+            <p>{product.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,23 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { FC } from 'react';
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
+  const pathname = usePathname();
+  if (pathname.startsWith('/studio')) return null;
   return (
     <div className=" flex justify-between p-10">
-      <Link href="#">Home</Link>
+      <Link href="/">Home</Link>
       <div>
         <div className="flex gap-6">
           <Link href="/login">Link1</Link>
           <Link href="/profile">Link2</Link>
+          {process.env.NODE_ENV === 'development' && (
+            <Link href="/studio">Studio</Link>
+          )}
         </div>
       </div>
     </div>
